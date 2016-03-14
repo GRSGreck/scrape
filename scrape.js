c5dbff8c7fd4e90aed6e2e88b6a77f56141d2b27
Roman <GrechukRomanSergeevich@gmail.com>
2016-03-14 23:11:12 +0200

    Comments made 100%

--- scrape.js
+++ scrape.js
@@ -8,6 +8,14 @@ var auth = {
     password: 'test'
 };
 
+/*
+ * Коннектимся к ресурсу по заданному url с помощью модуля "request"
+ * для получения доступа нам понадобится ввести login и password
+ * так же мы можем передать дополнительные параметры:
+ * - trans: 'train'
+ * - viev: 'get_country'
+ * - viev: 'group_country'
+ */
 request.post({
     url: url,
     form: {
@@ -19,30 +27,57 @@ request.post({
     }
 }, function(error, res, body) {
     if (!error && res.statusCode === 200) {
+
+        /*
+         * Используем модуль "cheerio" для работы с полученными данными (body),
+         * используя синтаксис, аналогичный jQuery
+         */
         var $ = cheerio.load(body);
         var $Item = $('root').find('item');
         var fileName = 'result.csv';
         var itemParse = {};
         var rowArr = [];
-        var pattern = /\<[a-zA-Z_]+?\>/gi;
+        var pattern = /<[a-zA-Z_]+?>/gi;
 
+        /*
+         * При каждом запуске приложения, если файл не существует то создаем,
+         * если файл существует то делаем полную очистку его содержимого
+         */
         fs.writeFile(fileName, '', function(error) {
             if (error) {
                 console.error(error.message);
             } else {
 
+                /*
+                 * Парсим данные отдельно по каждому елементу "item"
+                 * полученному из сервера и записываем их в файл "result.csv"
+                 */
                 $Item.each(function(index, element) {
                     var $element = $(element);
 
+                    /*
+                     * С помощью регулярного выражения сохраненного в переменной "pattern"
+                     * в полученном ответе ($Item) в цикле, поэлементно ($element) вытаскиваем имена
+                     * интересующих нас тегов и наполняем объект "itemParse" соответсвующими
+                     * данными
+                     */
                     while (match = pattern.exec($element.html())) {
                         var tagName = match[0].replace('<', '').replace('>', '');
                         itemParse[tagName] =  $element.find(tagName).text();
                     }
 
+                    /*
+                     * Полученный раннее из цикла объект "itemParse" пропускаем через
+                     * цикл for..in и копируем значения его свойств в массив "rowArr"
+                     */
                     for (var property in itemParse) {
                         rowArr.push(itemParse[property]);
                     }
 
+                    /*
+                     * При каждом цикле добавляем соответствующую строку данных в файл "result.csv"
+                     * путем преобразования массива в строку ([a, b, c].toString() => a,b,c)
+                     */
                     fs.appendFile(fileName, rowArr.toString() + '\n', 'utf8', function(error) {
                         if (error) {
                             console.error(error);
